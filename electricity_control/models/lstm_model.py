114aa3e68cec6f33d853d136132772c2ac30c34d
EvGe22 <iliya.strelnikov@yahoo.com>
2019-04-09 12:25:01 +0300

    change pytorch to tf, start experimenting

--- electricity_control/models/lstm_model.py
+++ electricity_control/models/lstm_model.py
@@ -1,35 +1,39 @@
+import pickle
+
+from keras.engine.saving import model_from_json
+
+import os
 from .model import Model
+import numpy as np
 from loguru import logger
-import torch
-from torch import nn
+from typing import Dict, Any
+import keras
+import json
 
-
-# TODO fill the code here with the model code
-class PyTorchLSTM(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, input):
-        pass
-
-    @classmethod
-    def load(cls, path_to_weights):
-        model = cls()
-        state_dict = torch.load(path_to_weights)
-        model.load_state_dict(state_dict)
-        return model
+columns = ['Global_active_power', 'Global_reactive_power', 'Voltage', 'Global_intensity',
+           'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3']
 
 
 class LSTMModel(Model):
 
-    def __init__(self, path_to_weights):
-        self.path_to_weights = path_to_weights
+    def __init__(self, path_to_weights_folder):
+        self.path_to_weights_folder = path_to_weights_folder
         self.model = None
+        self.scaler = None
 
-    def predict(self, data):
-        logger.info(f'Got {data}')
-        return data['Global_active_power'] + 0.5
+    def predict(self, data: Dict[str, Any]):
+        data = np.array([data[k] for k in columns])
+        data = np.expand_dims(data, 0)
+        return
 
     def load(self):
-        self.model = PyTorchLSTM.load(self.path_to_weights)
+        model_structure_path = os.path.join(self.path_to_weights_folder, 'model.json')
+        model_weights_path = os.path.join(self.path_to_weights_folder, 'weights.hd5')
+        scaler_path = os.path.join(self.path_to_weights_folder, 'scaler.pkl')
+        with open(model_structure_path, 'r') as f:
+            model_json = json.load(f)
+        self.model = model_from_json(model_json)
+        self.model.load_weights(model_weights_path)
+        self.model.compile(loss='mean_squared_error', optimizer='adam')
+        with open(scaler_path, 'rb') as f:
+            self.scaler = pickle.load(f)
