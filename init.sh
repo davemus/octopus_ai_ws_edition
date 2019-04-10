86845079adb7365a78ee36758c49d22cdaa965c5
Victor <victor@example.com>
2019-04-10 08:09:32 +0300

    download data

---
+++ init.sh
@@ -0,0 +1,8 @@
+mkdir -p data
+mkdir -p models
+wget "https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip" -O ./data/household_power_consumption.zip
+unzip -d data data/household_power_consumption.zip
+rm data/household_power_consumption.zip
+#wget "asd" -O models/models.zip
+#unzip -d models models/models.zip
+#rm models/models.zip
